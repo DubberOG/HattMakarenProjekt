fe3f89a702ef3f3a828a9881eb0b9ee0a057681b
Wille203 <stribergswille@hotmail.com>
2024-04-04 13:49:37 +0200

    Test

---
+++ src/Validering.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author William
+ */
+public class Validering {
+    //Kolla om det är tomt
+    //Eventuellt Lösenord String
+    //Epost string @ och .
+    //Textfält string
+    //Datum
+    //int
+    
+}
