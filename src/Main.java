7468e05ca458ebd08bde63fcb73521cae96c7196
DubberOG <154241815+DubberOG@users.noreply.github.com>
2024-04-04 13:58:44 +0200

    Test test

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+
+//Importer som kommer behöva göras för att ställa SQL-frågor
+import oru.inf.InfDB;
+import oru.inf.InfException;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
