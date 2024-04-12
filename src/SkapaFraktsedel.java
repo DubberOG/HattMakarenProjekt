cf0663b81cf3761e44cdd0868ead8475a9d24a4b
DubberOG <154241815+DubberOG@users.noreply.github.com>
2024-04-12 11:02:28 +0200

    test3

--- src/SkapaFraktsedel.java
+++ src/SkapaFraktsedel.java
@@ -54,7 +54,6 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
         lbOrderLista = new javax.swing.JLabel();
         lbValjKund = new javax.swing.JLabel();
         cbValjOrder = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -104,13 +103,6 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -139,9 +131,7 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
                                 .addComponent(txAngeVikt, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton1)
-                                    .addComponent(lbValjKund))
+                                .addComponent(lbValjKund)
                                 .addGap(0, 0, Short.MAX_VALUE)))))
                 .addGap(85, 85, 85)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -171,8 +161,6 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(cbValjOrder, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txAngeVikt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(88, 88, 88)
-                        .addComponent(jButton1)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
 
@@ -224,31 +212,6 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
         }
         
     }//GEN-LAST:event_btnSkapaActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
-    {
-        //SQL-fråga för att hämta OrderID från databasen
-         try{
-        ArrayList<HashMap<String, String>> allaOrderar = idb.fetchRows("SELECT OrderID FROM Orders WHERE Status = 'Redo'");
-                  
-        //Går igenom listan 
-                for(HashMap<String, String> order : allaOrderar){
-                
-            // Hämta orderId från listan och sparar det i en sträng
-            String orderID = order.get("OrderID");
-            
-            //Lägger till OrderID i comboboxen
-            txAInformation.append(orderID);    
-           
-                }}
-                    
-              
-        catch(InfException ettUndantag){
-            JOptionPane.showMessageDialog(null, "Databasfel!");
-         }
-    }
-    }//GEN-LAST:event_jButton1ActionPerformed
     
     private void avbrytFraktsedel()
     {
@@ -275,7 +238,7 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
             String orderID = order.get("OrderID");
             
             //Lägger till OrderID i comboboxen
-            cbValjOrder.addItem(orderID);    
+            cbValjOrder.addItem("orderID");    
            
                 }}
                     
@@ -330,7 +293,6 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
     private javax.swing.JButton btnAvbryt;
     private javax.swing.JButton btnSkapa;
     private javax.swing.JComboBox<String> cbValjOrder;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lbOrderLista;
