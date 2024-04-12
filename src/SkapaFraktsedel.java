3fc7f2bbf1bf9e189fbf538bc372457c181f8732
DubberOG <154241815+DubberOG@users.noreply.github.com>
2024-04-12 11:05:28 +0200

    Små fix

--- src/SkapaFraktsedel.java
+++ src/SkapaFraktsedel.java
@@ -30,6 +30,7 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
         initComponents();
         try {
             idb = new InfDB("Hattmakaren", "3306", "hattmakaren","HTM123");
+            fyllICombobox();
         } catch (InfException ex) {       
             JOptionPane.showMessageDialog(null, "Kunde inte ansluta till databasen!");
         }
@@ -172,7 +173,6 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
     }//GEN-LAST:event_btnAvbrytActionPerformed
 
     private void cbValjOrderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbValjOrderActionPerformed
-      fyllICombobox();
         //Kollar om comboboxen är tom
         if (cbValjOrder.getItemCount() == 0)
         {
@@ -238,7 +238,7 @@ public class SkapaFraktsedel extends javax.swing.JFrame {
             String orderID = order.get("OrderID");
             
             //Lägger till OrderID i comboboxen
-            cbValjOrder.addItem("orderID");    
+            cbValjOrder.addItem(orderID);    
            
                 }}
                     
